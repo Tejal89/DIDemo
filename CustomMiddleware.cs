82338808eecf50a15cd4b0151d5a766a967fc251
Tejal Chaudhari <tejal.github@gmail.com>
2024-04-28 13:23:25 +0530

    initial web api project files via file upload

---
+++ CustomMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using DIDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomMiddleware> _logger;
+
+        public CustomMiddleware(RequestDelegate next, ILogger<CustomMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public Task Invoke(HttpContext httpContext, IScopedService scopedService)
+        {
+            _logger.LogInformation("Scoped Service from Middleware : " + scopedService.GetScopedMessage());
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
